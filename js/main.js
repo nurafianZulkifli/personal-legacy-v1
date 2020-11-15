f648a9f79ec6c4d42a17c79a1d4e935c14c55f78
_nrfz <63917225+nurafianZulkifli@users.noreply.github.com>
2020-11-15 15:27:06 +0800

    Added About

--- js/main.js
+++ js/main.js
@@ -97,3 +97,18 @@ $(window).load(function(){
   });
 
 })
+
+
+$(document).ready(function(){
+  var my_posts = $("[rel=tooltip]");
+  for(i=0;i<my_posts.length;i++){
+      the_post = $(my_posts[i]);
+      if(the_post.hasClass('invert')){
+          the_post.tooltip({ placement: 'left'});
+          the_post.css("cursor","pointer");
+      }else{
+          the_post.tooltip({ placement: 'rigt'});
+          the_post.css("cursor","pointer");
+      }
+  }
+});
